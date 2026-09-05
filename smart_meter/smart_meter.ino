83aa8ae78804987723b869cda3715bffbb5a8dea
ByteJoseph <josephnjoseph43@gmail.com>
2026-09-05 12:10:24 +0530

    read pin D34

--- smart_meter/smart_meter.ino
+++ smart_meter/smart_meter.ino
@@ -1,9 +1,15 @@
-void setup() {
-  // put your setup code here, to run once:
+#define ACS_PIN 34
 
+void setup() {
+  Serial.begin(115200);
+  analogReadResolution(12);  // 0–4095
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  int adcValue = analogRead(ACS_PIN);
 
+  Serial.print("ACS ADC: ");
+  Serial.println(adcValue);
+
+  delay(500);
 }
