f51369da2cc07a51665ff367594a28f9360c1f82
ByteJoseph <josephnjoseph43@gmail.com>
2026-09-05 12:16:09 +0530

    read light from pin D32

--- smart_meter/smart_meter.ino
+++ smart_meter/smart_meter.ino
@@ -1,5 +1,5 @@
 #define ACS_PIN 34
-
+#define CIRC_STATE_PIN 32
 void setup() {
   Serial.begin(115200);
   analogReadResolution(12);  // 0–4095
@@ -7,9 +7,10 @@ void setup() {
 
 void loop() {
   int adcValue = analogRead(ACS_PIN);
-
+  int lightValue = analogRead(CIRC_STATE_PIN);
   Serial.print("ACS ADC: ");
   Serial.println(adcValue);
-
+  Serial.print("LIGHT: ");
+  Serial.println(lightValue);
   delay(500);
 }
